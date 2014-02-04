3c04bda250cbe31ee5b69a3ce9c15beb13f916fb
currupipi <pisterrassa@gmail.com>
2014-02-04 23:40:44 +0100

    Ahora si coño! faltaba el dhcp

--- cambia_mac.py
+++ cambia_mac.py
@@ -4,6 +4,7 @@
 
 import os, sys, subprocess
 from termcolor import colored
+from time import sleep
 
 #Comprueba si eres root
 def eresRoot():
@@ -38,7 +39,21 @@ def gestionaInterfaz(accion, interfaz=None):
             return 0
         except:
             return -1
-        
+    elif accion == 'mimac':
+        mac = '00:23:18:69:69:69'
+        indica_mac = "ifconfig wlan0 | grep HWaddr | cut -d ' ' -f10 | tr a-z A-Z"
+        salida = subprocess.check_output(indica_mac, shell=True)
+        salida = salida.rstrip('\n')
+        if  salida != mac :
+            print colored ('no se ha podido cambiar la mac :-(', 'red')
+            return 1
+        else:
+            return 0
+
+    else:
+        print colored ('Error desconocido en la llamada a gestionaInterfaz :-(', 'red')
+        return -1 
+
 #Cambia la mac
 def cambiaMac(interfaz=None, mac=None):
     if interfaz is None:
@@ -48,6 +63,15 @@ def cambiaMac(interfaz=None, mac=None):
     cambia_mac = 'ifconfig ' + interfaz + ' hw ether ' + mac
     try:
         salida = subprocess.check_call(cambia_mac, shell=True)
+        return 0
+    except:
+        return -1
+
+#Lanza DHCP
+def dhcp():
+    pide_dhcp = 'dhclient wlan0'
+    try:
+        salida = subprocess.check_call(pide_dhcp, shell=True)
         return 0
     except:
         return -1
@@ -73,10 +97,16 @@ def main():
         sys.exit(3)
     print colored ('Levantando de nuevo la interfaz', 'green')
     if gestionaInterfaz('enciende') == 0:
-        print colored ('Interfaz reinicada!', 'green')
+        print colored ('Interfaz reinicada! esto tarda unos segundos...', 'green')
+        sleep(10)
+        if gestionaInterfaz('mimac') == 1:
+            print colored ('Pero finalente no se ha cambiado correctamente la mac ...', 'red')
     else:
         print colored ('No se ha podido reiniciar la interfaz despues del cambio', 'red')
-        sys.exit(4)
+        sys.exit(40)
+    if dhcp() != 0:
+        print colored ('ERROR al pedir dhcp','red')
+    
     print colored ('****************************************************', 'blue')
 if __name__ == "__main__":
     main()
