d94fb1eed7fc416d4940022a31ba3f00660a0322
Currupipi <pisterrassa@gmail.com>
2014-09-21 19:11:15 +0200

    First function for network framework, get IP from interface)

--- network_fw.py
+++ network_fw.py
@@ -4,9 +4,23 @@
 
 import os, sys, subprocess
 
+#Gets the ip address of the specified interface, if not specified by default is wlan0
+def get_ip(interface=None):
+    import netifaces
+    if interface is None:
+        interface = 'wlan0'
+    myaddress = netifaces.ifaddresses(interface)[netifaces.AF_INET][0]
+    #my_ip = myaddress.get('addr')
+    #my_netmask = myaddress.get('netmask')
+    return myaddress
 
-def get_my_ip():
-    import socket
-    output = socket.gethostbyname(socket.gethostname())
-    return output
 
+def main():
+    print "testing values of my address"
+    my_addr = get_ip()
+    my_ip = my_addr.get('addr')
+    my_netmask = my_addr.get('netmask')
+    print "My ip is: ", my_ip , " and my netmask is: ", my_netmask
+
+if __name__ == "__main__":
+    main()
